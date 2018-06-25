de31da3e878a535d1b8278ebbbc60892cb2ad560
fenghuan <1207018406@qq.com>
2018-06-25 14:27:48 +0800

    d

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production';
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -53,8 +52,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ],
   output: {
